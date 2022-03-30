f2e93ddc0a7ec8e53490253d048482fda04aa806
Sungmin Oh <saogig0503@naver.com>
2022-03-30 16:11:15 +0900

    webpack config

--- 2. 끝말잇기/webpack.config.js
+++ 2. 끝말잇기/webpack.config.js
@@ -1,3 +1,15 @@
+const path = require('path');
+
 module.exports = {
-  
+  name: 'wordrelay-setting',
+  mode: 'development', // 실 서비스 : production
+  devtool: 'eval',
+
+  entry: {
+    app: ['./client.jsx', './WordRelay.jsx'],
+  }, // 입력
+  output: {
+    path: path.join(__dirname, 'dist'), 
+    filename: 'app.js',
+  }, // 출력
 };
