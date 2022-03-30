b979fcfd3e582dd76699ff800760e83a6bd37ef1
Sungmin Oh <saogig0503@naver.com>
2022-03-30 16:13:29 +0900

    webpack configure

--- 2. 끝말잇기/webpack.config.js
+++ 2. 끝말잇기/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devtool: 'eval',
 
   entry: {
-    app: ['./client.jsx', './WordRelay.jsx'],
+    app: ['./client'],
   }, // 입력
   output: {
     path: path.join(__dirname, 'dist'), 
