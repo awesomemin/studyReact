38103b04ca53d3ce5a708e7a4315e110355ae433
Sungmin Oh <saogig0503@naver.com>
2022-03-31 19:02:34 +0900

    change class component => functional component (NumberBaseballGame)

--- 3. 숫자야구/Try.jsx
+++ 3. 숫자야구/Try.jsx
@@ -1,15 +1,12 @@
 const React = require('react');
-const { Component } = React;
 
-class Try extends Component {
-  render() {
-    return (
-      <li>
-        <div>{this.props.tryInfo.try}</div>
-        <div>{this.props.tryInfo.result}</div>
-      </li>
-    )
-  }
+const Try = (props) => {
+  return (
+    <li>
+      <div>{props.tryInfo.try}</div>
+      <div>{props.tryInfo.result}</div>
+    </li>
+  )
 }
 
 module.exports = Try;
